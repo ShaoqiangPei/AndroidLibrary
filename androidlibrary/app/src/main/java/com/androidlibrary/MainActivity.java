6dcf0f91e651efd7eef9eeb82236607a6457ff2b
ShaoqiangPei <769936726@qq.com>
2019-01-14 15:50:21 +0800

    no message

--- androidlibrary/app/src/main/java/com/androidlibrary/MainActivity.java
+++ androidlibrary/app/src/main/java/com/androidlibrary/MainActivity.java
@@ -3,20 +3,12 @@ package com.androidlibrary;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.android.commonlibrary.util.AppUtil;
-import com.android.commonlibrary.util.LogUtil;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LogUtil.setDebug(true);
-
-        String source=AppUtil.getSourceApkPath();
-        LogUtil.i("======source=="+source);
 
     }
 
