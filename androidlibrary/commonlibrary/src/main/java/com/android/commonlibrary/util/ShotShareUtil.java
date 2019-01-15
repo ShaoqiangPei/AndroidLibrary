3cbd5b841665c9d9f597fd12d0048f22ed282084
ShaoqiangPei <769936726@qq.com>
2019-01-15 17:21:52 +0800

    添加截屏分享工具类

---
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/util/ShotShareUtil.java
@@ -0,0 +1,85 @@
+package com.android.commonlibrary.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Title:截屏分享
+ * Description:
+ *   需要用户读写权限
+ *   <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+ *   <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+ *
+ * Created by pei
+ * Date: 2017/12/6
+ */
+public class ShotShareUtil {
+
+    /***
+     * 截屏分享，供外部调用
+     *
+     * 图片路径：SDCardUtil.getDiskCachePath()+fileName
+     *
+     * @param fileName 图片名称,如："share.png"
+     * @param context
+     */
+    public static void shotShare(String fileName,Context context){
+        if(StringUtil.isNotEmpty(fileName)) {
+            //截屏
+            String path = screenShot(fileName, context);
+            //分享
+            if (StringUtil.isNotEmpty(path)) {
+                ShareImage(context, path);
+            }
+        }
+    }
+
+    /**
+     * 获取截屏
+     *
+     * @param fileName 图片名称,如："share.png"
+     * @param context
+     * @return
+     */
+    private static String screenShot(String fileName,Context context){
+        String imagePath=null;
+        Bitmap bitmap= ScreenUtil.snapShotWithoutStatusBar(context);
+        if(bitmap!=null){
+            try {
+                // 图片文件路径
+                imagePath = SDCardUtil.getDiskCachePath()+fileName;
+                File file = new File(imagePath);
+                FileOutputStream os = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
+                os.flush();
+                os.close();
+                return imagePath;
+            } catch (Exception e) {
+                LogUtil.e("====screenshot:error====" + e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    /**分享**/
+    private static void ShareImage(Context context, String imagePath){
+        if (imagePath != null){
+            Intent intent = new Intent(Intent.ACTION_SEND); // 启动分享发送的属性
+            File file = new File(imagePath);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));// 分享的内容
+            intent.setType("image/*");// 分享发送的数据类型
+            Intent chooser = Intent.createChooser(intent, "Share screen shot");
+            if(intent.resolveActivity(context.getPackageManager()) != null){
+                context.startActivity(chooser);
+            }
+        } else {
+            ToastUtil.shortShow("先截屏，再分享");
+        }
+    }
+
+}
