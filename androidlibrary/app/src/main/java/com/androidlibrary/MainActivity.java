fd7e9a45cc1783f88f4343cd0cdf484a81c5ce21
ShaoqiangPei <769936726@qq.com>
2019-03-25 19:01:30 +0800

    优化权限申请工具类

--- androidlibrary/app/src/main/java/com/androidlibrary/MainActivity.java
+++ androidlibrary/app/src/main/java/com/androidlibrary/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         mBtnTest=findViewById(R.id.button);
         mTvTest=findViewById(R.id.textView);
-
 
     }
 
