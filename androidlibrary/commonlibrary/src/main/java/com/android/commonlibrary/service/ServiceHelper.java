f8900278a6f740960422c317394a698cf9839620
LAPTOP-4NDLVHMD\pain <769936726@qq.com>
2019-11-18 11:23:16 +0800

    1. 增加非绑定式服务帮助类

---
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/service/ServiceHelper.java
@@ -0,0 +1,31 @@
+package com.android.commonlibrary.service;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Context;
+
+/**
+ * Title:服务帮助类
+ * description:
+ * autor:pei
+ * created on 2019/11/17
+ */
+public class ServiceHelper {
+
+    protected final int SERVICE_FRONT = 0x1988;
+
+    /**开启服务前台模式**/
+    public void startForeService(Service service, Notification notification, Context context){
+        if(service!=null&&notification!=null) {
+            service.startForeground(SERVICE_FRONT, notification);
+        }
+    }
+
+    /**关闭服务前台模式**/
+    public void stopForeService(Service service){
+        if(service!=null) {
+            service.stopForeground(true);
+        }
+    }
+
+}
---
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/service/local_service/LocalServiceHelper.java
@@ -0,0 +1,82 @@
+package com.android.commonlibrary.service.local_service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.commonlibrary.service.ServiceHelper;
+
+/**
+ * Title:"非绑定式"服务帮助类
+ * description:
+ * autor:pei
+ * created on 2019/11/17
+ */
+public class LocalServiceHelper extends ServiceHelper {
+
+    private Intent mLocalIntent;//“非绑定式"服务意图
+
+    /***
+     * 启动"非绑定式"服务
+     * @param context
+     * @param serviceCls
+     * @param action 要与服务在manifast.xml中注册的<action android:name=“xxx”/> 中的 xxx 值保持一致
+     */
+    public void startLocalService(Context context, Class<?>serviceCls, String action){
+        //需要注意的是：setAction中的参数要与服务在manifast.xml中注册的<action android:name=“xxx”/> 中的 xxx 值保持一致
+        mLocalIntent=new Intent(context, serviceCls);
+        mLocalIntent=startLocalServiceByIntent(context,mLocalIntent,serviceCls,action);
+    }
+
+
+    /***
+     * 关闭"非绑定式"服务
+     */
+    public void stopLocalService(Context context){
+        stopLocalServiceByIntent(context,mLocalIntent);
+    }
+
+    /***
+     * 启动"非绑定式"服务(可设置intent传参)
+     * @param context
+     * @param intent
+     * @param serviceCls
+     * @param action
+     * @return
+     */
+    public Intent startLocalServiceByIntent(Context context,Intent intent,Class<?>serviceCls,String action){
+        if(intent!=null){
+            intent.setAction(action);
+            context.startService(intent);
+        }
+        return intent;
+    }
+
+    /**
+     * 关闭"非绑定式"服务(需要传intent)
+     */
+    public void stopLocalServiceByIntent(Context context,Intent intent){
+        if(intent!=null){
+            context.stopService(mLocalIntent);
+        }
+    }
+
+    /***
+     * "非绑定式"服务主动关闭自己
+     */
+    public void stopLocalBySelf(Service service){
+        if(service!=null) {
+            service.stopSelf();
+        }
+    }
+
+    /***
+     * "非绑定式"服务主动关闭自己
+     */
+    public void stopLocalSelfById(Service service,int startId){
+        if(service!=null) {
+            service.stopSelf(startId);
+        }
+    }
+
+}
