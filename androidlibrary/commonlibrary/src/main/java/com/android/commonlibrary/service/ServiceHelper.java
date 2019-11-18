95c9a6abc1e992fc2806f3b49de85c8d1c884887
LAPTOP-4NDLVHMD\pain <769936726@qq.com>
2019-11-18 12:26:20 +0800

    1. 优化ServiceHelper中的开启前台模式代码

--- androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/service/ServiceHelper.java
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/service/ServiceHelper.java
@@ -4,6 +4,12 @@ import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 
+import androidx.core.app.NotificationCompat;
+
+import com.android.commonlibrary.R;
+import com.android.commonlibrary.service.local_service.LocalServiceHelper;
+import com.android.commonlibrary.util.NotificationHelper;
+
 /**
  * Title:服务帮助类
  * description:
@@ -13,6 +19,24 @@ import android.content.Context;
 public class ServiceHelper {
 
     protected final int SERVICE_FRONT = 0x1988;
+
+    /**
+     * 开启前台运行模式(简易通知栏模式)
+     * @param drawableId 设置小图标,一般设置app的icon,样式如：R.mipmap.ic_launcher
+     */
+    public void startForeService(Service service,int drawableId,Context context){
+        if(service!=null&&context!=null&&drawableId!=0) {
+            NotificationHelper notificationHelper = new NotificationHelper();
+            //创建builder
+            NotificationCompat.Builder builder = notificationHelper.init(drawableId)//初始化,设置小图标,必须调用,一般设置app的icon
+                    .setTitle("")//设置标题
+                    .setContent("")//设置内容
+                    .createBuilder(context);//创建builder
+            Notification notification = builder.build();
+            //设置前台模式
+            startForeService(service, notification);
+        }
+    }
 
     /**开启服务前台模式**/
     public void startForeService(Service service, Notification notification){
