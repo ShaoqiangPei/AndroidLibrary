294e424ff01596c295e5532cac3e82bcea904b2d
LAPTOP-4NDLVHMD\pain <769936726@qq.com>
2020-02-04 18:56:33 +0800

    1.修改设置log打印开关的方式

--- androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/app/LibraryConfig.java
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/app/LibraryConfig.java
@@ -2,6 +2,8 @@ package com.android.commonlibrary.app;
 
 import android.app.Application;
 
+import com.android.commonlibrary.util.LogUtil;
+
 /**
  * Title:androidLibrary库初始化类
  * description:
@@ -11,6 +13,7 @@ import android.app.Application;
 public class LibraryConfig {
 
     private Application mApplication;
+    private boolean mDebug=false;//是否开启网络打印(默认不开启)
 
     private LibraryConfig(){}
 
@@ -23,8 +26,21 @@ public class LibraryConfig {
     }
 
     /**初始化赋值(在项目的自定义Application中初始化)**/
-    public void init(Application application){
+    public LibraryConfig init(Application application){
         this.mApplication=application;
+        return LibraryConfig.this;
+    }
+
+    /**
+     * 是否打开Log打印
+     *
+     * @param debug true:打开调试log,  false:关闭调试log
+     * @return
+     */
+    public void setDebug(boolean debug){
+        this.mDebug=debug;
+        //设置自定义打印开关
+        LogUtil.setDebug(mDebug);
     }
 
     /**获取项目上下文**/
@@ -33,6 +49,14 @@ public class LibraryConfig {
             throw new NullPointerException("====AndroidLibrary需要初始化：LibraryConfig.getInstance.init(Application application)===");
         }
         return mApplication;
+    }
+
+    /**
+     * 获取log打印开关
+     * @return true:打开调试log,  false:关闭调试log
+     */
+    public boolean isDebug(){
+        return mDebug;
     }
 
 }
