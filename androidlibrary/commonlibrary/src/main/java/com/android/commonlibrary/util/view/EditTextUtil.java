eb1085a3fc8b2a5649b50c97af658048bd57c85b
LAPTOP-4NDLVHMD\pain <769936726@qq.com>
2020-04-01 10:05:52 +0800

    1. 优化EditTextUtil中关于设置光标位置的方法

--- androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/util/view/EditTextUtil.java
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/util/view/EditTextUtil.java
@@ -1,9 +1,13 @@
 package com.android.commonlibrary.util.view;
 
+import android.os.Build;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
+
+import com.android.commonlibrary.util.AppUtil;
 import com.android.commonlibrary.util.StringUtil;
 
 /**
@@ -15,11 +19,20 @@ import com.android.commonlibrary.util.StringUtil;
 public class EditTextUtil {
 
     /**设置光标显示在输入框尾部**/
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public static void setEndEditTextCursor(EditText edtText){
         String value=edtText.getText().toString();
         if(StringUtil.isNotEmpty(value)){
+            int length=value.length();
+            int sdkVersion= AppUtil.getSDKVersion();
+            if(sdkVersion> Build.VERSION_CODES.LOLLIPOP||sdkVersion==Build.VERSION_CODES.LOLLIPOP){
+                int maxLength=getMaxLength(edtText);
+                if(maxLength>0){
+                    length = length>= maxLength ? maxLength : length;
+                }
+            }
             //将光标移至文字末尾
-            edtText.setSelection(value.length());
+            edtText.setSelection(length);
         }
     }
 
@@ -32,5 +45,15 @@ public class EditTextUtil {
     /**设置输入框最大输入长度**/
     public static void setEditMaxLength(EditText edtText,int maxLength){
         edtText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private static int getMaxLength(EditText editText) {
+        int max = 0;
+        InputFilter inputFilter[]=editText.getFilters();
+        if(inputFilter!=null&&inputFilter.length>0&& inputFilter[0]!=null&&(inputFilter[0] instanceof InputFilter.LengthFilter)){
+            max = ((InputFilter.LengthFilter) inputFilter[0]).getMax();
+        }
+        return max;
     }
 }
