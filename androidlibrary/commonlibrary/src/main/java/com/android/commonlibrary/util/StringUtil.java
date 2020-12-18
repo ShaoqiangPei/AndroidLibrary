93707b513c760a71961231110c9fc3143d162f78
Pain <769936726@qq.com>
2020-12-18 09:23:09 +0800

    1. add countMatched function in StringUtil

--- androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/util/StringUtil.java
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/util/StringUtil.java
@@ -139,6 +139,30 @@ public class StringUtil {
         return str;
     }
 
+    /**
+     * 统计某个字符在字符串中出现的次数
+     *
+     * @param message 目标字符串
+     * @param sub 目标子字符串
+     * @return
+     */
+    public static int countMatched(String message, String sub){
+        //判空，为空直接返回0，表示不存在
+        if(isEmpty(message)||isEmpty(sub)){
+            return 0;
+        }
+        int count = 0;
+        int index;
+        // 循环一次将出现的字符串索引向后移一位，计数器+1，并截取原字符串索引+1的字符串，
+        // 如“abcdeeeee”第一次循环找到“e”索引4，substring(4+1)方法截取后得到新的字符串“eeee”，
+        // 循环第二次找到“e”的索引0，substring(0+1)方法截取后得到新的字符串“eee”，，，以此类推
+        while((index = message.indexOf(sub)) > -1){
+            count++;
+            message = message.substring(index + 1);
+        }
+        return count;
+    }
+
     /**过滤汉字**/
     public static String filterChinese(String chin){
         return chin.replaceAll("[\\u4e00-\\u9fa5]", "");
