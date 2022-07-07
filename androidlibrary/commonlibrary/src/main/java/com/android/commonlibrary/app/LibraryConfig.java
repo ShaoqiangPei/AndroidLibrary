d34ba35357c30a5d320f4aceced6f12af6a220d7
ShaoqiangPei <769936726@qq.com>
2022-07-07 17:53:14 +0800

    1. 配置中添加是否打印屏幕基本信息的方法

--- androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/app/LibraryConfig.java
+++ androidlibrary/commonlibrary/src/main/java/com/android/commonlibrary/app/LibraryConfig.java
@@ -2,6 +2,7 @@ package com.android.commonlibrary.app;
 
 import android.app.Application;
 import com.android.commonlibrary.util.LogUtil;
+import com.android.commonlibrary.util.ScreenUtil;
 
 /**
  * Title:androidLibrary库初始化类
@@ -13,6 +14,7 @@ public class LibraryConfig {
 
     private Application mApplication;
     private boolean mDebug=false;//是否开启网络打印(默认不开启)
+    private boolean mShowScreenInfo=false; //是否打印设备屏幕信息(默认不开启)
 
     private LibraryConfig(){}
 
@@ -42,6 +44,25 @@ public class LibraryConfig {
         LogUtil.setDebug(mDebug);
     }
 
+    /**
+     * 是否打印设备屏幕信息
+     *
+     * 注:当 mDebug==true 且 showScreenInfo==true时,打印才生效
+     *    作为配置项，需要在 setDebug(boolean debug)方法之后调用
+     *
+     * @param showScreenInfo true:打印屏幕信息,  false:不打印屏幕信息
+     * @return
+     */
+    public void setShowScreenInfo(boolean showScreenInfo){
+        this.mShowScreenInfo=showScreenInfo;
+        //是否打印设备屏幕信息(当 mDebug==true && showScreenInfo==true时,打印才生效)
+        if(mDebug&&mShowScreenInfo){
+            ScreenUtil.printScreenInfo();
+        }
+    }
+
+
+
     /**获取项目上下文**/
     public Application getApplication() {
         if(mApplication==null){
@@ -56,6 +77,18 @@ public class LibraryConfig {
      */
     public boolean isDebug(){
         return mDebug;
+    }
+
+    /**
+     * 获取是否具备打印设备屏幕信息能力的状态
+     *
+     * @return true:已打开且具备打印屏幕信息能力,  false:不具备打印屏幕信息能力
+     */
+    public boolean isShowScreenInfo(){
+        if(mDebug){
+            return mShowScreenInfo;
+        }
+        return false;
     }
 
 }
